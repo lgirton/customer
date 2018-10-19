103284def7e77ddabfc98fb9387fa6ab1b8b4561
lgirton <lgirton@AH486669.corp.SE.sempra.com>
2018-10-18 17:38:14 -0700

    Added customer CRUD operations

---
+++ customer-api/src/main/java/com/sdgesi/customer/Customer.java
@@ -0,0 +1,22 @@
+package com.sdgesi.customer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String firstName;
+	
+	private String lastName;
+
+}
---
+++ customer-api/src/main/java/com/sdgesi/customer/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.sdgesi.customer;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+	
+	List<Customer> findByLastName(@Param("lastName") String lastName);
+
+}
